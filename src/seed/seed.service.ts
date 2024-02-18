80cefce37a1d7551993104a7b3feb634cbbdf9ea
HeiderHDev <heiderhrnndz@gmail.com>
2024-02-18 15:24:47 -0500

    feat: 🎸 Se instalo Axios

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SeedService {
+
+  executeSeed() {
+    console.log(fetch);
+    return 'This action executes seed';
+  }
+}
