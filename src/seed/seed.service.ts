18d84ec8060e61d095803e64a61eb5db82fcf6f3
HeiderHDev <heiderhrnndz@gmail.com>
2024-02-18 16:03:29 -0500

    feat: 🎸 Se implemneto la semilla y se usa axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/pokemon-response.interfaces';
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
 
-  executeSeed() {
-    console.log(fetch);
-    return 'This action executes seed';
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no:number = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
